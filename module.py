1388b05e23fb9be42cf1f844b5d5e898c7742ffa
Luis Quezada <que18028@uvg.edu.gt>
2021-08-19 18:55:07 -0600

    Json Parser and Topo getter

--- module.py
+++ module.py
@@ -1,6 +1,10 @@
 
 import json
 
-def jsonToDict(path)
+def jsonToDict(path):
 	with open(path) as json_file:
 		return json.load(json_file)
+
+def topoGetter(node,path):
+	info = jsonToDict(path)
+	return info[node]
